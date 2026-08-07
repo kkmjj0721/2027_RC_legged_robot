cd23784589b10df1bd9d76915eef3d6192408033
奈奈奈奈奈绪 <2171670754@qq.com>
2026-08-07 12:23:36 +0800

    Update exporter.py

--- legged_gym/utils/exporter.py
+++ legged_gym/utils/exporter.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 import copy
 import os
 from typing import Optional, Sequence
