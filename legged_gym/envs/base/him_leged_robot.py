7eda09c9102a612807ae2a8a11f6d41920cce42b
奈奈奈奈奈绪 <2171670754@qq.com>
2026-08-13 11:38:36 +0800

    Update him_leged_robot.py

--- legged_gym/envs/base/him_leged_robot.py
+++ legged_gym/envs/base/him_leged_robot.py
@@ -12,7 +12,7 @@ from torch import Tensor
 from typing import Tuple, Dict
 
 from legged_gym import LEGGED_GYM_ROOT_DIR
-from legged_gym.envs.base.base_task import BaseTask
+from legged_gym.envs.base.him_base_task import BaseTask
 from legged_gym.utils.terrain import Terrain
 from legged_gym.utils.math import quat_apply_yaw, wrap_to_pi, torch_rand_sqrt_float
 from legged_gym.utils.helpers import class_to_dict
